de3da2859170efabca7256b7171e5f63bf1e0004
sarkar niloy <sarkarniloy102@gmail.com>
2024-07-12 23:09:45 +0600

    chart

--- script.js
+++ script.js
@@ -3,6 +3,38 @@ document.getElementById('menuIcon').addEventListener('click', function() {
   menuContent.classList.toggle('hidden');
 });
 
+// for chart
+const xValues = [' ','jan','feb','mar','apr','may','jun','jul'];
+
+new Chart("myChart", {
+  type: "line",
+  data: {
+    labels: xValues,
+    datasets: [{ 
+      data: [8,1140,1060,1060,1070,1110,1330,2210,7830,2478],
+      borderColor: "red",
+      fill: false
+    }, { 
+      data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
+      borderColor: "green",
+      fill: false
+    }, { 
+      data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
+      borderColor: "blue",
+      fill: false
+    },
+    { 
+      data: [300,700,2000,6000,7000,3000,2000,7000,200,100],
+      borderColor: "orange",
+      fill: false
+    }]
+  },
+  options: {
+    legend: {display: false}
+  }
+});
+
+// for slider
 
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
